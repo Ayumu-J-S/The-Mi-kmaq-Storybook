8faf38a6d23eb09372418194c1887ad078f73b95
Ayumu-J-S <umoo00015@gmail.com>
2021-12-29 17:58:29 -0400

    Update Storybook.js
    Now Sotrybook.js picks a story depending on the user's selection in the bookselection page.

--- storybook/src/components/storybook/Storybook.js
+++ storybook/src/components/storybook/Storybook.js
@@ -3,20 +3,36 @@ import axios from "axios";
 import { RoutingContext } from "../../context/Routing";
 import "./Storybook.css";
 
+function findStoryArr(arr, target) {
+    for (let obj of arr) {
+        if (obj.title === target) {
+            return obj.filenames;
+        }
+    }
+    return null;
+}
+
 const Storybook = (props) => {
     const { storyName } = useContext(RoutingContext);
 
-    const [storyArr, setStroyArr] = useState([]);
+    const [storyArr, setStoryArr] = useState([]);
+    const [pageArr, setPageArr] = useState([]);
     const [pageCount, setPageCount] = useState(1);
 
     useEffect(() => {
         axios.get("/test").then((response) => {
-            setStroyArr(response.data.arr[0].filenames);
+            let arr = findStoryArr(response.data.arr, storyName);
+            setStoryArr(arr);
         });
     }, []);
+
+    useEffect(() => {
+        setPageArr(storyArr[pageCount - 1]);
+    }, [storyArr]);
+
     return (
         <div id="mainPane">
-            <div id="left-pane">{storyArr}</div>
+            <div id="left-pane">{pageArr}</div>
             <div id="right-pane">a</div>
         </div>
     );
