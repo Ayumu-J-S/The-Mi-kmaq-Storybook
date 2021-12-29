293aad3c505de297af149f47e6b6b74913f025ee
Ayumu-J-S <umoo00015@gmail.com>
2021-12-28 23:35:18 -0400

    Updated Storybook.js to fetch the data from server

--- storybook/src/components/storybook/Storybook.js
+++ storybook/src/components/storybook/Storybook.js
@@ -1,9 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
 import { RoutingContext } from "../../context/Routing";
+import "./Storybook.css";
 
 const Storybook = (props) => {
     const { storyName } = useContext(RoutingContext);
-    return <div>{storyName}</div>;
+
+    const [storyArr, setStroyArr] = useState([]);
+    const [pageCount, setPageCount] = useState(1);
+
+    useEffect(() => {
+        axios.get("/test").then((response) => {
+            setStroyArr(response.data.arr[0].filenames);
+        });
+    }, []);
+    return (
+        <div id="mainPane">
+            <div id="left-pane">{storyArr}</div>
+            <div id="right-pane">a</div>
+        </div>
+    );
 };
 
 export default Storybook;
